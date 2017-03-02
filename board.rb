b27cd7c89db383449ead223572114d3099d9adda
Muhammad Alam <alam@mmtutor.com>
2017-03-02 13:57:10 -0500

    fixed order of method in play_turn

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require_relative "tile"
+require "byebug"
 
 class Board
   def self.empty_grid
@@ -28,6 +29,7 @@ class Board
 
   def []=(pos, value)
     x, y = pos
+    debugger
     tile = grid[x][y]
     tile.value = value
   end
